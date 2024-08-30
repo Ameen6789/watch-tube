f0ad5b03e511dc3d8b2fe497bd808fe4b741560d
ameen6789 <azrucha@gmail.com>
2024-08-30 21:52:35 +0530

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    API_KEY1:"AIzaSyAURq7_HaeKOv8RoxYkbEMyiwCmx7BV8u0",
+    API_KEY2:"AIzaSyB0ZQ15i0zQm1CA3AvL_xhSWcE4TPdx-mU",
+    API_KEY3:"AIzaSyAkbsax81YuidhYQAuusP44MfDk8kaK8JQ"
+}
